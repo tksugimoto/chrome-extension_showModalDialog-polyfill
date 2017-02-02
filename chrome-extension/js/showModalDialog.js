285570063113fbf7feed63f7f23444e6b1d7949e
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-02-03 00:12:17 +0900

    [Update] dialogArgumentsに対応

--- chrome-extension/js/showModalDialog.js
+++ chrome-extension/js/showModalDialog.js
@@ -12,40 +12,53 @@
 	// 共通
 	const newWindowName = "___new_window_%extension_id%";
 	const returnValueContainerKey = "___returnValueConnector_%extension_id%";
-	const returnValueKey = "_returnValue";
 
 	// ポップアップ
 	const originalClose = window.close;
 	window.close = () => {
 		if (typeof window.returnValue !== "undefined") {
 			if (window.opener && window.opener[returnValueContainerKey]) {
-				window.opener[returnValueContainerKey][returnValueKey] = returnValue;
+				window.opener[returnValueContainerKey].returnValue = returnValue;
 			}
 		}
 		originalClose();
 	};
+	if (window.opener && window.opener[returnValueContainerKey]) {
+		window.dialogArguments = window.opener[returnValueContainerKey].dialogArguments;
+	}
 
 	// メインページ
 	document.addEventListener("DOMContentLoaded", () => {
 		let latestClickedElement = null;
 		let triggerElement = null;
 		let latestReturnValue = null;
+		let latestDialogArguments = null;
 		window[returnValueContainerKey] = new Proxy({}, {
 			set: (obj, key, value) => {
-				latestReturnValue = value;
-				const elem = triggerElement;
-				triggerElement = null;
-				if (elem) {
-					elem.click();
+				if (key === "returnValue") {
+					latestReturnValue = value;
+					const elem = triggerElement;
+					triggerElement = null;
+					if (elem) {
+						elem.click();
+					}
+				}
+			},
+			get: (obj, key) => {
+				if (key === "dialogArguments") {
+					const dialogArguments = latestDialogArguments;
+					latestDialogArguments = null;
+					return dialogArguments;
 				}
 			}
 		});
-		window.showModalDialog = (url, _, windowStyle = "") => {
+		window.showModalDialog = (url, dialogArguments, windowStyle = "") => {
 			if (latestReturnValue) {
 				const returnValue = latestReturnValue;
 				latestReturnValue = null;
 				return returnValue;
 			} else {
+				latestDialogArguments = dialogArguments;
 				triggerElement = latestClickedElement;
 				windowStyle = windowStyle.replace(/:/g, "=").replace(/;/g, ",").replace(/dialog/g, "");
 				window.open(url, newWindowName, windowStyle);
